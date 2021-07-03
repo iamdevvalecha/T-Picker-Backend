d15fdfcef8a027ba054066f8f3ad826cf3a2033c
Ayan Hazra <69693282+jesus7110@users.noreply.github.com>
2021-07-03 10:03:04 +0530

    change userSchema.method to userSchema.methods

--- models/user.js
+++ models/user.js
@@ -52,7 +52,7 @@ userSchema
     return this._password;
   });
 
-userSchema.method = {
+userSchema.methods = {
   authenticate: function (plainpassword) {
     return this.securePassword(plainpassword) === this.encry_password;
   },
