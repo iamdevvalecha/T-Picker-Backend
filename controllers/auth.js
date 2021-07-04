d313d9bb2665ef94a3a0c19d4b0595f522fa1713
Dev Valecha <71969867+iamdevvalecha@users.noreply.github.com>
2021-07-04 21:49:52 +0530

    fix

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,26 +1,12 @@
-const User = require("../models/user");
-
-exports.signup = async (req, res) => {
-  const { name,lastname, email, encry_password } = req.body;
-  User.findOne({ email }).exec((err, user) => {
-    if (user) {
-      return res.status(400).json({ error: "User already exist" });
-    }
-    let newUser = new User({ name, lastname, email, encry_password });
-    newUser.save((err, success) => {
-      if (err) {
-        return res.status(400).json({ error: err })
-      }
-      res.json({
-        message: "User Signup"
-      })
-    })
+exports.signup = (req, res) => {
+  console.log("REQ BODY", req.body);
+  res.json({
+    message: "signup route works!"
   });
 };
 
 exports.signout = (req, res) => {
-    res.json({
-      message: "User signout",
-    });
- };
-  
+  res.json({
+    message: "User signout"
+  });
+};
