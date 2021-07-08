131726644edf9b27568edcc1b4902e8a29eb7ac8
Utkarsh1504 <utkarshmishra73444@gmail.com>
2021-07-09 00:10:33 +0530

    controller for sign-in

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,6 +16,34 @@ exports.signup = (req, res) => {
   });
 };
 
+// controller for signin route
+exports.signin = (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email }, (err, user) => {
+    if (err) {
+      return res.status(400).json({
+        err: "not able to find user",
+      });
+    }
+    if (!user) {
+      return res.status(400).json({
+        err: "user not found",
+      });
+    }
+    if (user.password !== password) {
+      return res.status(400).json({
+        err: "wrong password",
+      });
+    }
+    res.json({
+      name: user.name,
+      email: user.email,
+      id: user._id
+    });
+  });
+};
+
 exports.signout = (req, res) => {
   res.json({
     message: "User signout",
