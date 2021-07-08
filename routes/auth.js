493eeb812821babea5b97ab34ee34eb8288e7e3b
Utkarsh1504 <utkarshmishra73444@gmail.com>
2021-07-08 22:56:00 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,18 @@
 var express = require("express");
 var router = express.Router();
 const { signup, signin, signout } = require("../controllers/auth");
+const { check } = require('express-validator');
 
-router.post("/signup", signup);
+router.post("/signup", [
+    "/user",
+    check("name")
+        .isLength({ min: 1 }),
+    check("email")
+        .isEmail(),
+    check("password", 'The password must be 5+ chars long and contain a number')
+        .isLength({ min: 5 }),
+    signup], signup);
+
 router.post("/signin", signin);
 router.get("/signout", signout);
 
