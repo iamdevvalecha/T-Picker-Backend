cd2adccccdfa561053155df0f5889da83953b72e
Dev Valecha <devvalecha07@gmail.com>
2021-07-09 15:39:27 +0530

    validation routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,20 @@
 const User = require("../models/user");
+const { check, validationResult } = require("express-validator");
 
 exports.signup = (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      error: errors.array()[0].msg
+    });
+  }
+
   const user = new User(req.body);
   user.save((err, user) => {
     if (err) {
       return res.status(400).json({
-        err: "not able to save user",
+        err: "NOT able to save user in DB"
       });
     }
     res.json({
@@ -18,6 +27,6 @@ exports.signup = (req, res) => {
 
 exports.signout = (req, res) => {
   res.json({
-    message: "User signout",
+    message: "User signout"
   });
 };
