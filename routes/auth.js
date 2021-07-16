adfcf1f6dbb1cc89770fb742c074df6987095273
Utkarsh1504 <utkarshmishra73444@gmail.com>
2021-07-16 15:26:41 +0530

    router for isSignedIn

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const { check, validationResult } = require("express-validator");
-const { signout, signup, signin } = require("../controllers/auth");
+const { signout, signup, signin, isSignedIn } = require("../controllers/auth");
 
 router.post(
   "/signup",
@@ -23,5 +23,5 @@ router.post(
   signin
 );
 router.get("/signout", signout);
-
+router.get("/isSignedIn", isSignedIn);
 module.exports = router;
